5aec211364355fcc8de9ac19165be9059ec4b8b5
puluter <pkmq24@qq.com>
2024-01-21 12:11:07 +0800

    优化总结生成，换用gpt-4

--- LLMUtils/jsonParser.py
+++ LLMUtils/jsonParser.py
@@ -1,7 +1,6 @@
 import json
-import os
+
 from openai import OpenAI
-from loguru import logger
 
 client = OpenAI(
     # This is the default and can be omitted
@@ -18,10 +17,10 @@ def contains_keywords(text):
     return False
 
 
-def prompt(indent, usr_content):
+def prompt(usr_content):
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo-16k",
-        # model = "gpt-4-1106-preview",
+        # model = "gpt-3.5-turbo-16k",
+        model="gpt-4-1106-preview",
         # model = "gpt-4",
         # model = "gpt-4-1106-preview",
         messages=[
@@ -34,9 +33,10 @@ def prompt(indent, usr_content):
                 "content": usr_content
             }
         ],
-        temperature=0.7,
-        top_p=1,
-        frequency_penalty=0
+        temperature=0.2,
+        max_tokens=4000,
+        top_p=0.75,
+        frequency_penalty=0.5
 
     )
     result = response.choices[0].message.content
@@ -44,97 +44,92 @@ def prompt(indent, usr_content):
         result = result.split("\n\n")[1].split("\n")
         if contains_keywords(result[0]):
             result = result[1:]
-        result = [" " * indent + item for item in result]
         result = "\n".join(result) + "\n"
-
-    logger.info(result)
+    print(result)
     return result
+
 def get_content(lst):
     if lst == []:
         return ""
     content = ""
     for item in lst:
-        title = (int(item["Grade"])-2) * " " + "-" + " " + item["Title"]
-        title = title.replace("\n","")
+        title = (int(item["Grade"])) * "#" + " " + item["Title"]
+        title = title.replace("\n", "")
         title = title + "\n"
-        response = prompt((int(item["Grade"])-1),item["Content"])
-        content = content + title + response
+        content = content + title + item["Content"]
         content = content + get_content(item["Below"])
     return content
-    
-        
-# def use_prompt(content):
-#     if isinstance(content, dict):
-#         if "Content" in content:
-#             content["Content"] = use_prompt(content["Content"])
-#         if "Below" in content:
-#             content["Below"] = use_prompt(content["Below"])
-#         return content
-#     elif isinstance(content, list):
-#         if content == []:
-#             return []
-#         else:
-#             return [use_prompt(x) for x in content]
-#     elif isinstance(content, str):
-#         return prompt(content)
-#     return
 
 
 def parse_json(file_name):
     try:
-        json_file = open(file_name,"r")
+        json_file = open(file_name, "r")
         file_content = json_file.read()
         parsed_data = json.loads(file_content)
         return parsed_data
     except json.JSONDecodeError as e:
         print(f"JSON解析错误: {e}")
         return None
+
+
+file_name = "./work_file"
+# 解析JSON字符串
+
+
 from tqdm import tqdm
+
+
 def parse_from_json(json_obj):
     parsed_result = json_obj
 
+    # try:
     article = {}
     article["Title"] = parsed_result["Title"]
     Content = []
     abstract = {}
     abstract["Title"] = "Abstract"
-    abstract["Outline"] = prompt(0, parsed_result["Abstract"])
+    abstract["Outline"] = prompt(parsed_result["Abstract"])
     Content.append(abstract)
     for item in tqdm(parsed_result["Content"]):
+
+        if item["Title"] == "REFERENCES":
+            continue
         content = {}
         content["Title"] = item["Title"]
-        content["Outline"] = prompt(0, item["Content"]) + get_content(item["Below"])
+        content["Outline"] = prompt(item["Content"] + get_content(item["Below"]))
         Content.append(content)
     article["Outline"] = Content
     with open("result.json", 'w', encoding="utf-8") as file:
         json.dump(article, file, indent=2, ensure_ascii=False, )
     return article
-
-if __name__ == "__main__":
-    file_name = "./work_file"
-    # 解析JSON字符串
-    parsed_result = parse_json(file_name)
-
-    #try:
-    article = {}
-    article["Title"] = parsed_result["Title"]
-    Content = []
-    abstract = {}
-    abstract["Title"] = "Abstract"
-    abstract["Outline"] = prompt(0,parsed_result["Abstract"])
-    Content.append(abstract)
-    for item in parsed_result["Content"]:
-        content = {}
-        content["Title"] = item["Title"]
-        content["Outline"] = prompt(0,item["Content"]) + get_content(item["Below"])
-        Content.append(content)
-    article["Outline"] = Content
-    with open("result.json", 'w', encoding="utf-8") as file:
-        json.dump(article, file, indent=2 ,ensure_ascii=False,)
     # except:
     #     pass
 
 
 
-    
-    
+
+"""
+You are an expert in reading academic papers.
+You will be given a paragraph of introduction from an academic paper.
+Your task is to carefully read the given paragraph, then comprehend it and provide a summary.
+Your output should include two parts: the outline and the translation.
+
+- Delete adv., adj., phrases which is not necessary.
+- Omit the subject and unimportant predicates in the sentence. 
+- Change english numbers into 阿拉伯数字. (from "two" to "2")
+- The summary should not be about stating the topic of this paragraph; rather, it should point out the viewpoints expressed in the text.
+
+### Instruction ###
+1. Outline: Switch the summary into outline format with "-" and proper indent level. You need to strictly limit the outline to within 100 words, even if it means omitting some information. Choose the most crucial content from the paragraph and provide output. 
+2. Translate: 
+translate the outline part to chinese. You need to consider the context of the outline within the paragraph. You should always strictly follow this rules when translating:
+'Transformer' to 'Transformer';
+'Token' to 'Token';
+'LLM' to '大语言模型';
+'Large Language Model' to '大语言模型'
+'Generative AI' to '生成式 AI';
+'zero-shot' to 'zero-shot';
+
+
+
+"""
